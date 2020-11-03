7386b43db84bb6289683771c719f3054ef3a1b4a
Kushagra Singh <59528754+Kabi4@users.noreply.github.com>
2020-11-03 14:01:37 +0530

    Update modules.js

--- modules.js
+++ modules.js
@@ -2,7 +2,7 @@ console.log(arguments);
 console.log(require('module').wrapper);
 const C = require('./SampleModule');
 const Calc2 = require('./SampleModules2');
-const Calc1 = new C();
+const Calc1 = new C(); 
 console.log(Calc1.add(2, 7));
 console.log(Calc2.add(2, 7));
 //CACHING
